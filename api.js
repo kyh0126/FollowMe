337103f92659c8f69df97c4adcc8adc0ea149a10
kyh0126 <75013282+kyh0126@users.noreply.github.com>
2021-02-20 14:10:01 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,13 @@
-alert("Connect Success !!");
+function kakaoserch(){
+
+		var city = $("#city").val()+" 관광지";
+		$.ajax({
+			method : "get",
+			url : "https://dapi.kakao.com/v2/search/web",
+			data : {query : city},
+			headers : {Authorization : "KakaoAK 04881251da5eef1bfac8e06705918a6d"}
+		})
+		.done(function(msg){
+			console.log(msg);
+		})
+	}
