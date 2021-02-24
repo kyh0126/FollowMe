e9ee9e41fa66dc944ae8766ecab77c898c0d1723
kyh0126 <75013282+kyh0126@users.noreply.github.com>
2021-02-24 23:19:34 +0900

    Update map.js

--- map.js
+++ map.js
@@ -507,30 +507,35 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 	    	console.log(event);
 		$("#city").val();
 		$("#city").val("동구");
+		testjson();
 	    	kakaoserch();
 	});   
 	kakao.maps.event.addListener(jungguPolygon, 'mousedown', function() { 
 		console.log(event);
 		$("#city").val();
 		$("#city").val("중구");
+		testjson();
 		kakaoserch();
 	});   
 	kakao.maps.event.addListener(seoguPolygon, 'mousedown', function() { 
 		console.log(event);
 		$("#city").val();
 		$("#city").val("서구");
+		testjson();
 		kakaoserch();
 	});   
 	kakao.maps.event.addListener(yuseongguPolygon, 'mousedown', function() { 
 		console.log(event);
 		$("#city").val();
 		$("#city").val("유성구");
+		testjson();
 		kakaoserch();
 	});   
 	kakao.maps.event.addListener(daedeokguPolygon, 'mousedown', function() { 
 		console.log(event);
 		$("#city").val();
 		$("#city").val("대덕구");
+		testjson();
 		kakaoserch();
 	});     
 	// 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
